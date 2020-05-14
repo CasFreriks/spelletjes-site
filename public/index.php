885509557ad63f45ac80a5ba89c8daf144cb8d02
CasFreriks <freriks.cas@gmail.com>
2020-05-14 19:57:06 +0200

    laatste dingetjes en bugs opgelost

--- public/index.php
+++ public/index.php
@@ -13,7 +13,23 @@
 </head>
 <body>
 
-<?php require_once("../private/shared/nav.php")?>
+<nav class="navbar sticky-top navbar-expand-lg navbar-dark" style="background-color: #0065A0;">
+    <a class="navbar-brand" href="index.php">Spelletjes-site</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<div class="colorLine"></div>
+
+
 
 <div class="container-fluid">
     <div class="row">
